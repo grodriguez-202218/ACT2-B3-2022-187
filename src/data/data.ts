7a01238c422bcd8b7b3a9136de07e39660a80da5
grodriguez-2022187 <grodriguez-2022187@kinal.edu.gt>
2026-06-19 19:35:59 -0600

    feat: definir las fuentes de datos y colecciones base en data.ts

---
+++ src/data/data.ts
@@ -0,0 +1,13 @@
+export interface Productos{
+    nombre: string;
+    precio: number;
+    cantidad: number;
+}
+
+export const carrito: Productos[] = [
+  { nombre: "Camisa", precio: 125.5, cantidad: 2 },
+  { nombre: "Pantalón", precio: 220.0, cantidad: 1 },
+  { nombre: "Cinturón", precio: 85.0, cantidad: 1 },
+];
+
+export const TASA_IVA = 0.12;
